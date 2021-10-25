f52c985317440237bc16a487207a5807ac0a61b2
danielkomes <danielkomes@gmail.com>
2021-10-25 14:55:40 -0300

    Separate lockers for games and clients

--- Domain/Sys.cs
+++ Domain/Sys.cs
@@ -7,24 +7,25 @@ namespace Domain
         public static int IdCounter { get; set; }
         public static List<Game> Games { get; set; }
         public static List<Client> Clients { get; set; }
-        private static object locker;
+        private static object gamesLocker;
+        private static object clientsLocker;
 
         static Sys()
         {
-            locker = new object();
+            gamesLocker = new object();
             Games = new List<Game>();
             Clients = new List<Client>();
         }
         public static Client GetClient(string username)
         {
-            lock (locker)
+            lock (clientsLocker)
             {
                 return Clients.Find(c => c.Username.Equals(username));
             }
         }
         public static bool AddClient(string username)
         {
-            lock (locker)
+            lock (clientsLocker)
             {
                 bool ret = false;
                 Client c = new Client(username);
@@ -38,7 +39,7 @@ namespace Domain
         }
         public static void DeleteClient(string username)
         {
-            lock (locker)
+            lock (clientsLocker)
             {
                 Client c = GetClient(username);
                 Clients.Remove(c);
@@ -46,7 +47,7 @@ namespace Domain
         }
         public static int GetNewId()
         {
-            lock (locker)
+            lock (gamesLocker)
             {
                 return IdCounter++;
             }
@@ -54,7 +55,7 @@ namespace Domain
 
         public static void AddGame(Game game)
         {
-            lock (locker)
+            lock (gamesLocker)
             {
                 game.Id = GetNewId();
                 Games.Add(game);
@@ -62,21 +63,24 @@ namespace Domain
         }
         public static void DeleteGame(Game game)
         {
-            lock (locker)
+            lock (gamesLocker)
             {
                 Games.Remove(game);
-                foreach (Client c in Clients)
+                lock (clientsLocker)
                 {
-                    c.OwnedGames.Remove(game.Id);
+                    foreach (Client c in Clients)
+                    {
+                        c.OwnedGames.Remove(game.Id);
+                    }
                 }
             }
         }
         public static bool BuyGame(string username, int id)
         {
-            lock (locker)
+            bool ret = false;
+            Client client = GetClient(username);
+            lock (gamesLocker)
             {
-                bool ret = false;
-                Client client = GetClient(username);
                 if (Games.Find(g => g.Id == id) != null)
                 {
                     if (!client.OwnedGames.Contains(id))
@@ -85,30 +89,24 @@ namespace Domain
                         ret = true;
                     }
                 }
-                return ret;
             }
+            return ret;
         }
         public static bool RemoveGameFromClient(string username, int id)
         {
-            lock (locker)
-            {
-                bool ret = false;
-                Client client = GetClient(username);
-                ret = client.OwnedGames.Remove(id);
-                return ret;
-            }
+            bool ret = false;
+            Client client = GetClient(username);
+            ret = client.OwnedGames.Remove(id);
+            return ret;
         }
         public static void RemoveAllGamesFromClient(string username)
         {
-            lock (locker)
-            {
-                Client c = GetClient(username);
-                c.OwnedGames.Clear();
-            }
+            Client c = GetClient(username);
+            c.OwnedGames.Clear();
         }
         public static void ReplaceGame(Game game)
         {
-            lock (locker)
+            lock (gamesLocker)
             {
                 for (int i = 0; i < Games.Count; i++)
                 {
@@ -122,7 +120,7 @@ namespace Domain
         }
         public static void AddReview(int id, Review review)
         {
-            lock (locker)
+            lock (gamesLocker)
             {
                 Game g = Games.Find(g => g.Id == id);
                 if (g != null)
