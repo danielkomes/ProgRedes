4e9b1df8d125166cfef0f30b048535159242a635
danielkomes <danielkomes@gmail.com>
2021-11-15 19:00:44 -0300

    restAPI test

---
+++ ApiClient/Controllers/ValuesController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ApiClient.Controllers
+{
+    [Route("pets")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        public static List<Pet> pets = new List<Pet>();
+
+        [HttpGet]
+        public IEnumerable<Pet> getAllPets()
+        {
+            return pets;
+        }
+
+        [HttpPost]
+        public Pet Save([FromBody] Pet pet)
+        {
+            pets.Add(pet);
+            return pet;
+        }
+    }
+
+    public class Pet
+    {
+        public string name { get; set; }
+
+    }
+}
