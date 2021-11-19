f70aba2815d3c2c5c140d53e77b41f1d09b7715a
danielkomes <danielkomes@gmail.com>
2021-11-19 20:52:34 -0300

    Create Subscriber project

---
+++ Subscriber/SubscriberProgram.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace Subscriber
+{
+    class SubscriberProgram
+    {
+        private const string ExchangeName = "n6aRoutedExchange";
+        private const string RoutingKey = "even";
+
+        static void Main(string[] args)
+        {
+            ConnectionFactory factory = new ConnectionFactory { HostName = "localhost" };
+            using IConnection connection = factory.CreateConnection();
+            using IModel channel = connection.CreateModel();
+            DeclareExchange(channel);
+            string queueName = channel.QueueDeclare().QueueName;
+            DeclareQueue(channel, queueName);
+            ReceiveMessages(channel, queueName);
+            Console.WriteLine("This will get even length messages");
+            Console.WriteLine("Press enter to exit.");
+            Console.ReadLine();
+        }
+
+        private static void DeclareExchange(IModel channel)
+        {
+            channel.ExchangeDeclare(exchange: ExchangeName, type: ExchangeType.Direct);
+        }
+
+        private static void DeclareQueue(IModel channel, string queueName)
+        {
+            channel.QueueBind(
+                queue: queueName,
+                exchange: ExchangeName,
+                routingKey: RoutingKey);
+        }
+
+        private static void ReceiveMessages(IModel channel, string queueName)
+        {
+            var consumer = new EventingBasicConsumer(channel);
+            consumer.Received += (model, ea) =>
+            {
+                byte[] body = ea.Body.ToArray();
+                string message = Encoding.UTF8.GetString(body);
+                Console.WriteLine("Received message : " + message);
+            };
+
+            channel.BasicConsume(
+                queue: queueName,
+                autoAck: true,
+                consumer: consumer);
+        }
+    }
+}
